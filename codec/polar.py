0f7e9bb40472babec1f8b25e65e5d41b897cc7d1
ivan chung <ivan0703@gmail.com>
2018-08-09 01:22:52 +0800

    20180808 result

--- codec/polar.py
+++ codec/polar.py
@@ -3,36 +3,59 @@ import math
 
 class Polar:
 
-    def __init__(self, N, K, ch_param, frozen):
+    @staticmethod
+    def reverse(x, n):
+        result = 0
+        for i in range(n):
+            if (x >> i) & 1: result |= 1 << (n - 1 - i)
+        return result
+
+    def __init__(self, N, K):
         self.ch_type = 'bsc'
         self.N = N
         self.K = K
         self.n = math.ceil(math.log2(N))
-        self.p = ch_param
-        # TODO: determine frozen bits
-        self.frozen = frozen
+        self.LLR = np.zeros(2*N - 1)
+        self.BITS = np.zeros((2, N - 1))
+        self.bit_reversed_idx = np.zeros(N)
+
+        for i in range(N):
+            self.bit_reversed_idx[i] = self.reverse(i, self.n)
+
+
 
     # Construct frozenbits
-    def construct(self, N, K, ch_param):
+    def construct(self, N, K, channel):
         return
 
     def encode(self, msg_bits):
         pass
 
-    def decode(self, y):
+    def decode(self, llr_y, frozen):
         """
         Successive decoding
         :return:
         """
-        pass
+        u = np.zeros(len(llr_y))
+        self.LLR[0:N] = 0
+        self.LLR[self.N-1:] = llr_y
+
+        return u
 
 
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     N = 8
     K = 4
     p = 0.1
     frozen = [0, 0, 0, -1, 0, -1, -1, -1]
+    llr_y = [1, 0, 1, 0, 0, 1, 0, 0]
 
-    polar = Polar()
-    polar.construct(N, K, p, frozen)
+    polar = Polar(N, K)
+    print(f'Polar({polar.N},{polar.K})')
+    print(f'bit reverse (0~7): {polar.bit_reversed_idx}')
+
+    u = polar.decode(llr_y, frozen)
+    print(f'{polar.LLR}')
+    print(f'u={u}')
