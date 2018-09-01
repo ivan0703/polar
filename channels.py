62eb98d78280a42d63cbbce0fc815cfdc4644386
ivan chung <ivan0703@gmail.com>
2018-09-02 01:51:15 +0800

    channel bsc done

--- channels.py
+++ channels.py
@@ -1,10 +1,29 @@
 import numpy as np
 
-def bsc():
-    pass
+__all__=['bsc', 'bec', 'awgn']
+
+def bsc(in_bits, p):
+    """
+
+    :param in_bits: Input bits
+    :param p: Channel
+    :return: Output bits
+    """
+    out_bits = in_bits.copy()
+    flip_locs = (np.random.random(len(out_bits)) <= p)
+    out_bits[flip_locs] = 1 ^ out_bits[flip_locs]
+    return out_bits
 
 def bec():
     pass
 
 def awgn():
     pass
+
+
+if __name__ == '__main__':
+    bsc_p = 0.1
+    in_bits = np.array([0,0,1,0,1,1,0,0]);
+    out_bits = bsc(in_bits,bsc_p)
+
+    print(f'Output buts = {out_bits}')
